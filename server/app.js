0322e5f7507202dd0c56c668692fb0311d8ec6e4
AKSO <justinaung786@gmail.com>
2026-05-30 21:10:42 +0630

    remove extra spaces

--- server/app.js
+++ server/app.js
@@ -2,13 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const calcService = require("./services/calcService");
 
-
 const app = express();
 app.use(express.json());
 
 app.post("/add", async (req, res) => {
     const { a, b } = req.body;
-
     const result = await calcService.add(a, b);
 
     res.json({
@@ -32,5 +30,5 @@ app.post("/sub", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("🚀 Server running on http://localhost:3000");
+    console.log("Server running on http://localhost:3000");
 });
